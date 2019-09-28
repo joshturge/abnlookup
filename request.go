693c9bf9bb471b3f123951972be69a1f91565f97
joshturge <joshturge@hotmail.com>
2019-09-28 22:18:47 +1000

    Added SearchByABN

---
+++ request.go
@@ -0,0 +1,43 @@
+package abnlookup
+
+import (
+	"encoding/xml"
+	"fmt"
+	"net/url"
+)
+
+func (c *Client) SearchByABN(abn string, history bool) (*ABRPayloadSearchResults, error) {
+	var includeHistory string
+	if history {
+		includeHistory = "Y"
+	} else {
+		includeHistory = "N"
+	}
+
+	v := url.Values{}
+	v.Add("searchString", abn)
+	v.Add("includeHistoricalDetails", includeHistory)
+
+	req, err := c.NewRequest("SearchByABNv201408", v)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't create new request: %s", err)
+	}
+
+	var ABRPSR ABRPayloadSearchResults
+	resp, err := c.Do(req, &ABRPSR)
+	if err != nil {
+		return nil, err
+	}
+
+	// If the usage statement isn't defined then there was probably an exception
+	if ABRPSR.Response.UsageStatement == "" {
+		var ABRPSRException ABRPayloadSearchResultsException
+		if err = xml.NewDecoder(resp.Body).Decode(&ABRPSRException); err != nil {
+			return nil, fmt.Errorf("couldn't decode response body into ResponseException: %s", err)
+		}
+
+		return nil, fmt.Errorf(ABRPSRException.ExceptionResponse.Exception.String())
+	}
+
+	return &ABRPSR, nil
+}
