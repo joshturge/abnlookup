1309e2b056eda3262cf3d1aed07bba3916e71e3c
joshturge <joshturge@hotmail.com>
2019-09-29 13:55:56 +1000

    Added comments and verbose error handling

--- client.go
+++ client.go
@@ -12,7 +12,8 @@ import (
 	"time"
 )
 
-// Client holds information about a http client
+// Client holds information about a http client and GUID
+// which is used for API authentication
 type Client struct {
 	BaseURL    *url.URL
 	GUID       string
@@ -20,11 +21,13 @@ type Client struct {
 	logger     *log.Logger
 }
 
-// NewClient will create a new instance of client with GUID provided
+// NewClient will create a instance of client with GUID you provide.
+// If you don't have a GUID yet, you can register for one here:
+// https://api.gov.au/service/5b639f0f63f18432cd0e1a66/Registration
 func NewClient(guid string) (*Client, error) {
 	base, err := url.Parse("https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("couldn't pass the base url: %s", err.Error())
 	}
 
 	logger := log.New(os.Stdout, "[ABN-LOOKUP] ", 0)
@@ -43,24 +46,37 @@ func (c *Client) SetTimeout(duration time.Duration) {
 	c.httpClient.Timeout = duration
 }
 
-// NewRequest creates a new request instance
-func (c *Client) NewRequest(path string, form url.Values) (*http.Request, error) {
-	form.Add("authenticationGuid", c.GUID)
+// NewRequest creates a new request instance, your GUID will be added
+// to your url.Values and the host header is set within this function.
+func (c *Client) NewRequest(path string, urlVal url.Values) (*http.Request, error) {
 
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s?%s", c.BaseURL.String(), path, form.Encode()), nil)
+	urlVal.Add("authenticationGuid", c.GUID)
+
+	// Generate the url
+	var url bytes.Buffer
+	url.WriteString(c.BaseURL.String())
+	url.WriteString(path)
+	url.WriteString("?")
+	url.WriteString(urlVal.Encode())
+
+	// Create a new GET request, body is nil as the values needed for an API request are encoded within the url
+	req, err := http.NewRequest("GET", url.String(), nil)
 	if err != nil {
 		return nil, err
 	}
 
-	req.PostForm = form
+	// Redundant?
+	/* req.PostForm = form
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded") */
+
 	req.Header.Set("Host", "abr.business.gov.au")
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	return req, nil
 }
 
-// Do will execute a http request and decode the response body into a struct
+// Do will execute a http request and decode a response body into a valid struct
 func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
+	// For debugging purposes
 	c.logger.Println(req.Method, req.URL.String())
 
 	resp, err := c.httpClient.Do(req)
@@ -68,22 +84,34 @@ func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 		return nil, err
 	}
 
-	if resp.StatusCode != 200 {
-		return resp, fmt.Errorf("client response status was not 200: Got %d: %s", resp.StatusCode, http.StatusText(resp.StatusCode))
+	if resp.StatusCode != http.StatusOK {
+		var body []byte
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return resp, fmt.Errorf("couldn't read from response body: %s", err.Error())
+		}
+		return resp, fmt.Errorf("API response status was not 200: Got %d: %s: Response.Body: %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(body))
 	}
 
-	// This is definently not ideal for big response bodies but it's all I know at this point
+	// This is definently not ideal for big response bodies but the API
+	// responses I've seen so far are only 1-2KB max in size.
+	// A better option would be to use io.Copy() but I'm not too sure
+	// how I would implement it.
 	// Get a copy of the response body as I need to have resp.Body available
-	bodyByte, err := ioutil.ReadAll(resp.Body)
+	var bodyByte []byte
+	bodyByte, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("couldn't read from response body: %s", err.Error())
 	}
-
+	// Set resp.Body to a new ReadCloser with same response body
 	resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyByte))
+
+	// Create a Reader from bodyByte so it can be decoded
 	body := bytes.NewReader(bodyByte)
 
+	// Decode response body into struct
 	if err = xml.NewDecoder(body).Decode(v); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("couldn't decode response body into struct: %s", err.Error())
 	}
 
 	return resp, nil
