c115872762b72c277e7c33672f33b03c62365e78
joshturge <joshturge@hotmail.com>
2019-09-29 13:56:44 +1000

    Added comments and added Error method

--- exception_response.go
+++ exception_response.go
@@ -5,11 +5,16 @@ import (
 	"fmt"
 )
 
+// Exception describes an exception and provides an exception code.
+// More information about exceptions and there meaning can be found
+// here: https://api.gov.au/service/5b639f0f63f18432cd0e1a66/Exceptions#exception-codes-and-descriptions
 type Exception struct {
 	Description string `xml:"exceptionDescription"`
 	Code        string `xml:"exceptionCode"`
 }
 
+// ExceptionResponse is a response received from the API that has
+// an Exception tag
 type ExceptionResponse struct {
 	XMLName                 xml.Name  `xml:"response"`
 	UsageStatement          string    `xml:"usageStatement"`
@@ -18,12 +23,15 @@ type ExceptionResponse struct {
 	Exception               Exception `xml:"exception"`
 }
 
+// ABRPayloadException holds a Request and an ExceptionResponse
+// which holds information on a request
 type ABRPayloadException struct {
 	XMLName           xml.Name          `xml:"ABRPayloadSearchResults"`
 	Request           Request           `xml:"request"`
 	ExceptionResponse ExceptionResponse `xml:"response"`
 }
 
-func (exc Exception) String() string {
-	return fmt.Sprintf("exception code: %s description: %s", exc.Code, exc.Description)
+// Error will return a formatted string with information about an API exception
+func (abrpe *ABRPayloadException) Error() string {
+	return fmt.Sprintf("exception code: %s description: %s", abrpe.ExceptionResponse.Exception.Code, abrpe.ExceptionResponse.Exception.Description)
 }
