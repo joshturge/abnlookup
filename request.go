806b2e947bf7183c1e7f4a12c27e983619625b0f
joshturge <joshturge@hotmail.com>
2019-09-30 21:37:14 +1000

    Updated request methods

--- request.go
+++ request.go
@@ -8,7 +8,7 @@ import (
 
 // SearchByABN will return a ABRPayload with search results for a specified ABN or will return an error
 // Also does a validity check on the provided ABN
-func (c *Client) SearchByABN(abn string, history bool) (*ABRPayloadSearchResults, error) {
+func (c *Client) SearchByABN(abn string, history bool) (*ABRPayloadBusinessResults, error) {
 	if ValidateABN(abn) {
 		return c.searchBy("SearchByABNv201408", abn, history)
 	}
@@ -16,7 +16,7 @@ func (c *Client) SearchByABN(abn string, history bool) (*ABRPayloadSearchResults
 }
 
 // SearchByASIC will return a ABRPayload with search results for a specified ASIC or return an error
-func (c *Client) SearchByASIC(asic string, history bool) (*ABRPayloadSearchResults, error) {
+func (c *Client) SearchByASIC(asic string, history bool) (*ABRPayloadBusinessResults, error) {
 	if ValidateACN(asic) {
 		return c.searchBy("SearchByASICv201408", asic, history)
 	}
@@ -28,7 +28,7 @@ func (c *Client) SearchByASIC(asic string, history bool) (*ABRPayloadSearchResul
 // into a ABRPayloadSearchResults struct, if the Response UsageStatement is not set then this
 // function will then try to decode the response body into a ABRPayloadException struct and
 // return an error.
-func (c *Client) searchBy(searchType string, query string, history bool) (*ABRPayloadSearchResults, error) {
+func (c *Client) searchBy(searchType string, query string, history bool) (*ABRPayloadBusinessResults, error) {
 	// The Lookup API requires a string for includeHistoricalDetails that can either be 'Y' or 'N'
 	// true == "Y", false == "N".
 	var includeHistory string
@@ -50,14 +50,14 @@ func (c *Client) searchBy(searchType string, query string, history bool) (*ABRPa
 	}
 
 	// Do the request and decode the response body into an ABRPayloadSearchResults struct
-	var ABRPSR ABRPayloadSearchResults
-	resp, err := c.Do(req, &ABRPSR)
+	var ABRPBR ABRPayloadBusinessResults
+	resp, err := c.Do(req, &ABRPBR)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't do request: %s", err.Error())
 	}
 
 	// If the usage statement isn't defined then there was probably an exception
-	if ABRPSR.Response.UsageStatement == "" {
+	if ABRPBR.Response.UsageStatement == "" {
 		var ABRPException ABRPayloadException
 		if err = xml.NewDecoder(resp.Body).Decode(&ABRPException); err != nil {
 			return nil, fmt.Errorf("couldn't decode response body into ABRPayloadException: %s", err)
@@ -66,5 +66,5 @@ func (c *Client) searchBy(searchType string, query string, history bool) (*ABRPa
 		return nil, fmt.Errorf(ABRPException.Error())
 	}
 
-	return &ABRPSR, nil
+	return &ABRPBR, nil
 }
