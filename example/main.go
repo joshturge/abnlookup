adc2920d39515ba43ac0d3d0f2588c2ab6120952
joshturge <joshturge@hotmail.com>
2019-10-01 10:36:57 +1000

    Updated example

--- example/main.go
+++ example/main.go
@@ -14,17 +14,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	results, err := client.SearchByABN("49 093 669 660", false)
+	abnResults, err := client.SearchByABN("49 093 669 660", false)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("ACN Number: %s\n", results.Response.BusinessEntity.ASICNumber)
+	fmt.Printf("ACN Number: %s\n", abnResults.BusinessEntityResponse.BusinessEntity.ASICNumber)
 
-	if abnlookup.ValidateACN(results.Response.BusinessEntity.ASICNumber) {
+	if abnlookup.ValidateACN(abnResults.BusinessEntityResponse.BusinessEntity.ASICNumber) {
 		fmt.Println("ACN is valid")
 	} else {
 		fmt.Println("ACN is not valid")
 	}
+
+	nameQuery := abnlookup.NameQuery{
+		Name:           "Glen",
+		Postcode:       "4156",
+		LegalName:      true,
+		TradingName:    true,
+		BusinessName:   true,
+		ActiveABNsOnly: false,
+		StateCodes:     []string{"QLD"},
+		SearchWidth:    "typical",
+		MinimumScore:   20,
+		MaxResults:     10,
+	}
+
+	personResults, err := client.SearchByName(nameQuery)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(personResults.PersonEntityResponse.SearchResults.PersonEntity[0].ABN.IdentiferValue)
 }
